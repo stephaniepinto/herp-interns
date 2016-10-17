a30c33721d12da90675847db62355e05c4d99f04
Carolyn Sy <csy@bc-lap-047.wireless.internal.amnh.org>
2016-10-17 16:09:15 -0400

    python shell

---
+++ week2/substitutions.py
@@ -0,0 +1,52 @@
+# substitutions.py
+def count_bases(sequence):
+	"""
+	Returns a count of how many of each nucleotide (A,C,T,G) exist in the DNA sequence.
+	"""
+	pass
+
+def complement(sequence):
+	"""
+	Returns the reverse complement of a sequence of DNA.
+	"""
+	pass
+
+def transcribe_dna(sequence):
+	"""
+	Returns a string representing the mRNA transcript of a DNA sequence
+	"""
+	pass
+
+def translate_rna(sequence):
+	"""
+	Returns a string representing the amino acid translation of an mRNA
+	sequence
+	"""
+	pass
+
+
+# this may be for the second day
+
+def random_substitution(sequence):
+	"""
+	Replaces a random base position in the input string with a different base, 
+	and returns the new string, simulating a subsitution mutation.
+	"""
+	pass
+
+def is_synonymous(sequence1, sequence2):
+	"""
+	Return True if the two DNA sequences produce the same amino acid sequence,
+	and False otherwise. 
+	"""
+	pass
+
+def count_synonymous(sequence, num_trials=100):
+	"""
+	Run 100 trials in which we simulate a random substitution, and count how 
+	many of the random substitutions result in a nonsynonymous vs
+	synonymous(silent) mutation.
+	"""
+	pass
+
+
