428bda9ec73ce7e645ef234ed779b377f551865a
Stephanie <stephaniepinto13@gmail.com>
2016-11-01 18:34:55 -0400

    added python review

---
+++ 1101_pythonpractice_SP.py
@@ -0,0 +1,16 @@
+def square (x):
+	return (x * x)
+
+a = square(13)
+print a
+
+
+def favorite_color(name): 
+	names = {'Ava' : 'blue', 'Crystal' : 'blue', 'Elena' : 'silver', 'Odelia' : 'none', 'Renee' : 'purple'}
+	print names[name]
+	return
+
+favorite_color(raw_input(''))
+
+def reverse (list):
+	list 
