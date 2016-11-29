c25aa3a59797739880bc894d0927b992beb525b8
Stephanie <stephaniepinto13@gmail.com>
2016-11-29 17:26:40 -0500

    testing

--- 1101_pythonpractice_SP.py
+++ 1101_pythonpractice_SP.py
@@ -1,16 +1,22 @@
-def square (x):
-	return (x * x)
+# def square (x):
+# 	return (x * x)
 
-a = square(13)
-print a
+# a = square(13)
+# print a
 
 
-def favorite_color(name): 
-	names = {'Ava' : 'blue', 'Crystal' : 'blue', 'Elena' : 'silver', 'Odelia' : 'none', 'Renee' : 'purple'}
-	print names[name]
-	return
+# def favorite_color(name): 
+# 	names = {'Stephanie' : 'blue',  'Ava' : 'blue', 'Crystal' : 'blue', 'Elena' : 'silver', 'Odelia' : 'none', 'Renee' : 'purple'}
+# 	print names[name]
+# 	return
 
-favorite_color(raw_input(''))
+# favorite_color(raw_input(''))
 
-def reverse (list):
-	list 
+def reverse (lst):
+	lst = [1,2,3,4]
+	new_lst = []
+	index = len(lst) -1
+	while index >= 0:
+		new_lst.append(lst[index])
+		index = index -1
+	return new_lst
--- week2/substitutions.py
+++ week2/substitutions.py
@@ -5,29 +5,71 @@ def count_bases(sequence):
 	"""
 	Returns a count of how many of each nucleotide (A,C,T,G) exist in the DNA sequence.
 	"""
-	pass
+	A = 0
+	T = 0
+	C = 0
+	G = 0
+	for i in sequence:
+		if i == "A":
+			A = A + 1
+		elif i == "T":
+			T = T + 1
+		elif i == "C":
+			C = C + 1
+		elif i == "G":
+			G = G + 1
+	print A , T, G, C
+
 
 def complement(sequence):
-	"""
-	Returns the reverse complement of a sequence of DNA.
-	"""
-	pass
+	new_string = ""
+	for i  in sequence:
+		if i == "A":
+			new_string = new_string + "T"
+		elif i == "T":
+			new_string = new_string + "A"
+		elif i == "C":
+			new_string = new_string + "G"
+		elif i == "G":
+			new_string = new_string + "C"
+	return new_string
+
+	
+
 
 def transcribe_dna(sequence):
-	"""
-	Returns a string representing the mRNA transcript of a DNA sequence
-	"""
-	pass
+	new_string = ""
+	for i  in sequence:
+		if i == "A":
+			new_string = new_string + "U"
+		elif i == "T":
+			new_string = new_string + "A"
+		elif i == "C":
+			new_string = new_string + "G"
+		elif i == "G":
+			new_string = new_string + "C"
+	return new_string
+
+
 
 def translate_rna(sequence):
-	"""
-	Returns a string representing the amino acid translation of an mRNA
-	sequence
-	"""
-	pass
 
+	AA = []
 
-# this may be for the second day
+	aminoAcids = {'UUU':'Phe', 'UUC':'Phe', 'UUA':'Leu', 'UUG':'Leu', 'UCU':'Ser','UCC':'Ser', 'UCA':'Ser', 'UCG':'Ser', 'UAU':'Tyr', 'UAC':'Tyr','UAA':'Stop', 'UAG':'Stop', 'UGU':'Cys', 'UGC':'Cys','UGA':'Stop', 'UGG':'Trp', 'CUU':'Leu', 'CUC':'Leu', 'CUA':'Leu','CUG':'Leu', 'CCU':'Pro', 'CCC':'Pro', 'CCA':'Pro', 'CCG':'Pro','CAU':'His', 'CAC':'His', 'CAA':'Gln', 'CAG':'Glyn', 'CGU':'Arg','CGC':'Arg','CGA':'Arg', 'CGG':'Arg', 'AUU':'Ile', 'AUC':'Ile','AUA':'Ile', 'AUG':'Met', 'ACU':'Thr', 'ACC':'Thr', 'ACA':'Thr','ACG':'Thr', 'AAU':'Asn', 'AAC':'Asn', 'AAA':'Lys', 'AAG':'Lys','AGU':'Ser', 'AGC':'Ser', 'AGA':'Arg', 'AGG':'Arg', 'GUU':'Val','GUC':'Val', 'GUA':'Val', 'GUG':'Val', 'GCU':'Ala', 'GCC':'Ala','GCA':'Ala', 'GCG':'Ala', 'GAU':'Asp', 'GAC':'Asp', 'GAA':'Glu','GAG':'Glu', 'GGU':'Gly', 'GGC':'Gly', 'GGA':'Gly', 'GGG':'Gly'}
+	
+	index = 0
+	for i in range(len(sequence)/3):
+		codon = ''
+		codon = codon + sequence[index]
+		codon = codon + sequence[index + 1]
+		codon = codon + sequence[index + 2]
+		AA.append(aminoAcids[codon])
+		index = index + 3
+
+	return AA
+	print AA
+
 
 def random_substitution(sequence):
 	"""
